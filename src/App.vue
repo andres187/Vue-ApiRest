8e6b932c81386d7fd2c5de37018c1eb5a6cfde85
Andres Bolivar <andresfebo187@gmail.com>
2019-02-08 23:19:07 -0500

    First commit task create and read all tasks

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      
+    <h2>Tasks</h2>
+      <div class="row">
+        <div class="col-sm">
+          <div v-if="loading">
+            <p>Loading</p>
+          </div>
+          <div v-else>
+            <task
+              v-for="item in items"
+              :key="item._id"
+              :description="item.description"
+              :author="item.author"
+            >
+            </task>
+            <br>
+          </div>
+        </div>
+        <div class="col-sm">
+          <task-form @save="create">
+          </task-form>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import Task from './components/Task';
+import TaskForm from './components/TaskForm';
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    'task': Task,
+    'task-form': TaskForm,
   },
+  data() {
+    return {
+      loading: true,
+      items: [],
+    }
+  },
+  created() {
+    fetch('http://localhost:3000/api/tasks')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const { items = [] } = data;
+        const tasks = items.map((item) => {
+          const { author = {} } = item;
+          const { firstname = 'Anonimous', lastname = '' } = author;
+          return {
+            description: item.description,
+            author: `${firstname} ${lastname}`,
+          }
+        });
+        this.items = tasks;
+        this.loading = false;
+      });
+  },
+  methods: {
+    create(task) {
+      fetch('http://localhost:3000/api/tasks', {
+        method: 'POST',
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.items.push(data);
+        });
+      },
+    },
+    
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="post__container">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-text">{{description}}</h5>
+        <p>{{author}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'post',
+  props: {
+    description: {
+      type: String,
+      default: '',
+    },
+    author: {
+      type: String,
+      required: true
+    },
+  },
+};
+</script>
+
+<style>
+    .post__container{
+        margin: 10px;
+    }
+</style>
